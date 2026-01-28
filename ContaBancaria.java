ecdef53934b6d0860ce55423434c6052e7821ed9
AdielsonMedeiros <medeirosadielson@gmail.com>
2026-01-28 01:51:24 -0300

    att

--- ContaBancaria.java
+++ ContaBancaria.java
@@ -1,3 +1,28 @@
 public class ContaBancaria {
     private double saldo;
+
+
+    public ContaBancaria(double saldoInicial){
+        this.saldo = saldoInicial;
+    }
+    
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public void depositar (double valor){
+        
+    }
+
+
+    public boolean sacar (double valor){
+        
+
+    }
+
+
+    public boolean sacar (double valor){
+    
+    }
+
 }
